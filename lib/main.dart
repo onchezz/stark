53c0efb4fad7bc1c52c6e24239bcf3b236952fe9
onchezz <brianonchezz@gmail.com>
2024-01-11 22:24:12 +0300

    updates

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:stark_app/test_packages/relayer_ui.dart';
+
+// import 'package:starknet/starknet.dart';
+// import 'package:starknet_flutter/starknet_flutter.dart';
+
+// Future<void> main() async {
+//   const nodeUri = 'https://starknet-goerli.infura.io/v3/872474efe7554d4d8891b17444ceb31a';
+//   await StarknetFlutter.init(
+//     nodeUri: (nodeUri.isNotEmpty) ? Uri.parse(nodeUri) : infuraGoerliTestnetUri,
+//   );
+
+//   runApp(const SmartApp());
+// }
+void main() {
+  runApp(const SmartApp());
+}
+
+class SmartApp extends StatelessWidget {
+  const SmartApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "StarkNet Wallet in Flutter",
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const Relayer(),
+    );
+  }
+}
---
+++ lib/test_packages/relayer_ui.dart
@@ -0,0 +1,161 @@
+// import 'package:account_connect/test_packages/relayer_service.dart';
+// ignore_for_file: avoid_print
+
+import './relayer_service.dart';
+import 'package:flutter/material.dart';
+import 'package:starknet/starknet.dart';
+
+class Relayer extends StatefulWidget {
+  const Relayer({super.key});
+
+  @override
+  State<Relayer> createState() => _RelayerState();
+}
+
+class _RelayerState extends State<Relayer> {
+  int _totalBalance = 0;
+  String _relayerWalletBal = "0";
+  int _relayerBal = 0;
+  final TextEditingController _amount = TextEditingController();
+
+  Future _getWalletBalance() async {
+    Uint256 bal = await getSignerWalletBal();
+    setState(() {
+      _relayerWalletBal = bal.toString();
+    });
+  }
+
+  Future _viewTotalVaultbal() async {
+    int bal = await getTotalVaultBalance();
+    setState(() {
+      _totalBalance = bal;
+    });
+  }
+
+  Future _getPersonalBal() async {
+    print('getting personal reayer balance');
+    int bal = await viewUserBalance();
+    setState(() {
+      _relayerBal = bal;
+    });
+    print('getting personal reayer balance $bal ');
+  }
+
+  Future _approveUsdcToken() async {
+    int amount = int.parse(_amount.text.trim());
+    print("approving usdc amount while regestering ");
+
+    await approveUsdcToken(amount);
+    // await regesteringUser(amount);
+    _amount.clear();
+  }
+
+  Future _regesterUser() async {
+    int amount = int.parse(_amount.text.trim());
+    print(" regestering ");
+
+    // await approveUsdcToken(amount);
+    await regesteringUser(amount);
+    _amount.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 120,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Available Total liquidity balance is : $_totalBalance"),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Text("relayer balance is : $_relayerBal"),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text("wallet ball $_relayerWalletBal"),
+              const SizedBox(
+                width: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: (() => _getWalletBalance()),
+                    child: const Text('Wallet bal'),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: (() => _viewTotalVaultbal()),
+                    child: const Text('Vault bal'),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: (() => _getPersonalBal()),
+                    child: const Text('relayer vault bal'),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: 500,
+                child: TextField(
+                  controller: _amount,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your Amount',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: (() => _regesterUser()),
+                    child: const Text('regester user'),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: (() => _approveUsdcToken()),
+                    child: const Text('Approve usdc'),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  //   ElevatedButton(
+                  //     onPressed: (() => _getPersonalBal()),
+                  //     child: const Text('relayer vault bal'),
+                  //   ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
